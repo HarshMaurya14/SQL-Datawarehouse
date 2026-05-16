ef3854eb488712c44c17e02eaa36e1f2a1f35d10
MAURYA HARSH RAKESH <m.harsh142005@gmail.com>
2026-05-17 00:46:56 +0530

    Update and rename clean_silver.sql to silver_clean.sql

--- scripts/sliver layer/silver_clean.sql
+++ scripts/sliver layer/silver_clean.sql
@@ -117,6 +117,47 @@ SELECT
 
 FROM bronze.crm_sales_details;
 
+INSERT INTO silver.erp_cust_az12(cid,bdate,gen)
+SELECT 
+    CASE 
+    WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+    ELSE cid
+END AS cid,
+CASE 
+   WHEN bdate > GETDATE() THEN NULL
+   ELSE bdate 
+   END AS bdate,
+   CASE
+      WHEN UPPER (TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
+      WHEN UPPER (TRIM(gen)) IN ('F','MALE') THEN 'male'
+      ELSE 'n/a'
+END AS gen
+FROM bronze.erp_cust_az12;
+
+INSERT INTO silver.erp_loc_a101
+(cid,cntry)
+SELECT 
+REPLACE(cid,'-','')cid,
+CASE 
+   WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+   WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+   ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101;
+
+INSERT INTO silver.erp_px_cat_g1v2
+(id,
+cat,
+subcat,
+maintenance)
+SELECT 
+id,
+cat,
+subcat,
+maintenance
+FROM bronze.erp_px_cat_g1v2;
+
+
 
 
 
